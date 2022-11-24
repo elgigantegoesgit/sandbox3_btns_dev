c2fdf777c800c777e47b90e08e5f1a9d951da094
elgig <el.gigante@gmx.net>
2022-11-24 19:41:54 +0000

    Update 5 files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// import MultiselectListbox from './vue-multiselect-listbox';
+// import './vue-multi-select-listbox.css';
+
+
 createApp(App).mount("#app");
